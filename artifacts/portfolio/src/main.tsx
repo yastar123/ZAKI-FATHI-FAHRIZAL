c2d57abf5a55e161aea39a2d1b533f76053bafa7
yastar123 <eduj2164@gmail.com>
2026-06-19 18:55:26 +0700

    Configure portfolio to use deployed Vercel API server

--- artifacts/portfolio/src/main.tsx
+++ artifacts/portfolio/src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { setBaseUrl } from "@workspace/api-client-react";
+
+// Configure API base URL to use deployed Vercel API server
+setBaseUrl("https://zaki-fathi-fahrizal-api-server.vercel.app");
 
 createRoot(document.getElementById("root")!).render(<App />);
